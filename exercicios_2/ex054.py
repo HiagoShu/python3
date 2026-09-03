0ced2a51e7f09208b3f5ebffd32ac8749a7c9538
Shu <hiagogabriel@alu.ufc.br>
2026-09-03 14:05:00 -0300

    Update ex054.py

--- exercicios_2/ex054.py
+++ exercicios_2/ex054.py
@@ -2,8 +2,8 @@
 #Considere a maioridade 21 
 from datetime import date
 atual = date.today().year
-for idade in range(1,8):
-    nasc = int(input('Em que ano a {}º pessoa nasceu? '.format(idade)))
+for pessoa in range(1,8):
+    nasc = int(input('Em que ano a {}º pessoa nasceu? '.format(pessoa)))
     idade = atual - nasc
     if idade < 21:
         print('Essa pessoa ainda não atingiu a maioridade.')
